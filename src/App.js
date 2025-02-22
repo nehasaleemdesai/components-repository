4a3686076c93847c60cae5130722b4190551177c
Neha Saleem Desai <nehasaleemdesai99@gmail.com>
2025-02-21 20:02:04 -0600

    updates in dropdown component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import Dropdown from "./components/Dropdown";
 
 const App = () => {
   const [selection, setSelection] = useState(null);
+  const [selectedFruit, setSelectedFruit] = useState(null);
 
   const handleSelect = (option) => {
     setSelection(option);
+  };
+
+  const handleFruitSelect = (option) => {
+    setSelectedFruit(option);
   };
 
   const options = [
@@ -14,10 +19,21 @@ const App = () => {
     { label: "Blue", value: "blue" },
   ];
 
+  const fruits = [
+    { label: "Apple", value: "Apple" },
+    { label: "Banana", value: "Banana" },
+    { label: "Mango", value: "Mango" },
+    { label: "Orange", value: "Orange" },
+  ];
+
   return (
     <div className="flex">
       <Dropdown options={options} value={selection} onChange={handleSelect} />
-      <Dropdown options={options} value={selection} onChange={handleSelect} />
+      <Dropdown
+        options={fruits}
+        value={selectedFruit}
+        onChange={handleFruitSelect}
+      />
     </div>
   );
 };
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,10 +1,29 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { GoChevronDown } from "react-icons/go";
 import Panel from "./Panel";
 
 function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false);
+  const divEl = useRef();
+
+  useEffect(() => {
+    const handler = (event) => {
+      if (!divEl.current) {
+        return;
+      }
+
+      if (!divEl.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handler, true);
+
+    return () => {
+      document.removeEventListener("click", handler, true);
+    };
+  }, []);
 
   const handleClick = () => {
     setIsOpen((currentIsOpen) => !currentIsOpen);
@@ -32,7 +51,7 @@ function Dropdown({ options, value, onChange }) {
   });
 
   return (
-    <div className="w-48 relative">
+    <div ref={divEl} className="w-48 relative">
       <Panel
         className="flex justify-between items-center cursor-pointer "
         onClick={handleClick}
