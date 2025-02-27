c7e6bc1d460d0b4668dcc4e33d8244da5d18a5d3
Neha Saleem Desai <nehasaleemdesai99@gmail.com>
2025-02-26 22:18:02 -0600

    started working on modal page

---
+++ src/pages/ModalPage.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Modal from "../components/Modal";
+import Button from "../components/Button";
+import { useState } from "react";
+
+function ModalPage() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClick = () => {
+    setShowModal(true);
+  };
+
+  return (
+    <div>
+      <Button onClick={handleClick} Primary>
+        Open Modal
+      </Button>
+      {showModal && <Modal />}
+    </div>
+  );
+}
+
+export default ModalPage;
