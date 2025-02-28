727b9fa05d3cd373052f1511c150cd4d8345653a
Neha Saleem Desai <nehasaleemdesai99@gmail.com>
2025-02-27 18:49:13 -0600

    modal component complete with styles

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,31 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import { useEffect } from "react";
 
-function Modal() {
-  return <div>Modal</div>;
+function Modal({ onClose, children, actionBar }) {
+  useEffect(() => {
+    document.body.classList.add("overflow-hidden");
+
+    return () => {
+      document.body.classList.remove("overflow-hideen");
+    };
+  }, []);
+
+  return ReactDOM.createPortal(
+    <div>
+      <div
+        onClick={onClose}
+        className="fixed inset-0 bg-gray-300 opacity-80"
+      ></div>
+      <div className="fixed inset-40 p-10 bg-white border-gray-300 rounded shadow-2xl">
+        <div className="flex flex-col justify-between h-full">
+          {children}
+          <div className="flex justify-end">{actionBar}</div>
+        </div>
+      </div>
+    </div>,
+    document.querySelector(".modal-container")
+  );
 }
 
 export default Modal;
--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -10,12 +10,31 @@ function ModalPage() {
     setShowModal(true);
   };
 
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  const actionBar = (
+    <div>
+      {
+        <Button onClick={handleClose} primary>
+          I Accept
+        </Button>
+      }
+    </div>
+  );
+  const modal = (
+    <Modal onClose={handleClose} actionBar={actionBar}>
+      <p>Here is an important agreement for you to accept</p>
+    </Modal>
+  );
+
   return (
     <div>
-      <Button onClick={handleClick} Primary>
+      <Button onClick={handleClick} primary>
         Open Modal
       </Button>
-      {showModal && <Modal />}
+      {showModal && modal}
     </div>
   );
 }
