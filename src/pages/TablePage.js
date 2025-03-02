d56cca2c63cf7b42ca152288e32b9f7ea2c5f53d
Neha Saleem Desai <nehasaleemdesai99@gmail.com>
2025-03-01 20:05:35 -0600

    created and updated table component

---
+++ src/pages/TablePage.js
@@ -0,0 +1,37 @@
+import Table from "../components/Table";
+
+import React from "react";
+
+function TablePage() {
+  const data = [
+    {
+      name: "Orange",
+      color: "bg-orange-500",
+      score: 5,
+    },
+    {
+      name: "Apple",
+      color: "bg-red-500",
+      score: 3,
+    },
+    {
+      name: "Banana",
+      color: "bg-yellow-500",
+      score: 1,
+    },
+    {
+      name: "Lime",
+      color: "bg-green-500",
+      score: 4,
+    },
+  ];
+
+  const config = [{ label: "Name" }, { label: "Color" }, { label: "Score" }];
+  return (
+    <div>
+      <Table data={data} config={config} />
+    </div>
+  );
+}
+
+export default TablePage;
