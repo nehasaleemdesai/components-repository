7ffc915df5c6207bad878581435f787a39285c1c
Neha Saleem Desai <nehasaleemdesai99@gmail.com>
2025-03-02 19:17:31 -0600

    Updates in Table page to remove hard-fixed values

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -26,10 +26,22 @@ function TablePage() {
     },
   ];
 
-  const config = [{ label: "Name" }, { label: "Color" }, { label: "Score" }];
+  const config = [
+    { label: "Name", render: (fruit) => fruit.name },
+    {
+      label: "Color",
+      render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
+    },
+    { label: "Score", render: (fruit) => fruit.score },
+  ];
+
+  const keyFn = (fruit) => {
+    return fruit.name;
+  };
+
   return (
     <div>
-      <Table data={data} config={config} />
+      <Table data={data} config={config} keyFn={keyFn} />
     </div>
   );
 }
