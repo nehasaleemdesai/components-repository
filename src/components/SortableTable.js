290c27dee090eae74069ea04c37861255593bfae
Neha Saleem Desai <nehasaleemdesai99@gmail.com>
2025-03-04 22:36:53 -0600

    adding sorting component for table

---
+++ src/components/SortableTable.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { useState } from "react";
+import Table from "./Table";
+
+function SortableTable(props) {
+  const [sortOrder, setSortOrder] = useState(null);
+  const [sortBy, setSortBy] = useState(null);
+  const { config } = props;
+
+  const handleClick = (label) => {
+    if (sortOrder === null) {
+      setSortOrder("asc");
+      setSortBy(label);
+    } else if (sortOrder === "asc") {
+      setSortOrder("desc");
+      setSortBy(label);
+    } else if (sortOrder === "desc") {
+      setSortOrder(null);
+      setSortBy(null);
+    }
+  };
+
+  const updatedConfig = config.map((column) => {
+    if (!column.sortValue) {
+      return column;
+    }
+    return {
+      ...column,
+      header: () => (
+        <th onClick={() => handleClick(column.label)}>
+          {column.label} IS SORTABLE
+        </th>
+      ),
+    };
+  });
+  return <Table {...props} config={updatedConfig} />;
+}
+
+export default SortableTable;
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,5 +1,6 @@
 import React from "react";
-import Table from "../components/Table";
+// import Table from "../components/Table";
+import SortableTable from "../components/SortableTable";
 
 function TablePage() {
   const data = [
@@ -26,7 +27,11 @@ function TablePage() {
   ];
 
   const config = [
-    { label: "Name", render: (fruit) => fruit.name },
+    {
+      label: "Name",
+      render: (fruit) => fruit.name,
+      sortValue: (fruit) => fruit.name,
+    },
     {
       label: "Color",
       render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
@@ -34,7 +39,7 @@ function TablePage() {
     {
       label: "Score",
       render: (fruit) => fruit.score,
-      header: () => <th className="bg-red-500">Score</th>,
+      sortValue: (fruit) => fruit.score,
     },
   ];
 
@@ -44,7 +49,7 @@ function TablePage() {
 
   return (
     <div>
-      <Table data={data} config={config} keyFn={keyFn} />
+      <SortableTable data={data} config={config} keyFn={keyFn} />
     </div>
   );
 }
